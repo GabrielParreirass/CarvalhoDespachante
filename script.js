f9b04c12aaa5e1b5d1a8210c2910ebb72dfbed45
Gabriel Parreiras <gparreiras46@gmail.com>
2023-03-31 23:50:15 -0300

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function handleClickMobile() {
+
+    const menuMobile = document.getElementById("menuMobile")
+
+
+    menuMobile.classList.toggle("hidden")
+    menuMobile.classList.toggle("show")
+
+
+}
+
+function handleCloseMobile() {
+    const menuMobile = document.getElementById("menuMobile")
+
+
+    menuMobile.classList.toggle("hidden")
+    menuMobile.classList.toggle("show")
+}
